f521b1dacfdadec99143bcda0f56730cd1d00a0a
FedeAngeriz <10086@holbertonstudents.com>
2025-04-26 22:31:52 -0300

    update

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """Define list sum function with type float and return their sum as a float."""
 
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
     return sum(input_list)
