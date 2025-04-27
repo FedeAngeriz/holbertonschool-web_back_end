a1df9a61187639c2fcbfcc4cdd85dd314f0030a6
FedeAngeriz <10086@holbertonstudents.com>
2025-04-26 22:11:59 -0300

    update file

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Compare this snippet from python_variable_annotations/4-sum.py:
 def add(a: float, b: float) -> float:
     """Returns the sum of two floats."""
     return a + b
