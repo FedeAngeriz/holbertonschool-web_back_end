10b09a454362c63640530aaece20501af84d4e0c
FedeAngeriz <10086@holbertonstudents.com>
2025-04-26 22:53:08 -0300

    update 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Create a function that multiplies a float by a given multiplier."""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     
     def multiply(number: float) -> float:
         """Multiply the input number by the multiplier."""
