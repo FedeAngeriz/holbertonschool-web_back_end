48497ed7f8db579882a73aaffbdf81e58440be18
FedeAngeriz <10086@holbertonstudents.com>
2025-05-01 22:17:31 -0300

    update

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Async comprehension that yields 10 random numbers"""
 from typing import List
 import random
 async_generator = __import__('0-async_generator').async_generator
