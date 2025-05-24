329512a921717c18d6aead4282a7c64415566030
FedeAngeriz <10086@holbertonstudents.com>
2025-05-24 16:55:33 -0300

    update files

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+""" Import wait_random from 0-basic_async_syntax.py and run it in a loop """
 import asyncio
-from 0-basic_async_syntax import wait_random
-"""Import wait_random from 0-basic_async_syntax.py and run it in a loop"""
+from typing import List
+import importlib
+wait_random = importlib.import_module("0-basic_async_syntax").wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    """wait_n that takes in 2 int arguments (n, max_delay) and returns a list of all the delays (float values)"""
-    
+    """ should return the list of all the delays """
+    delay = []
+    for i in range(n):
+        delay.append(wait_random(max_delay))
+
+    return delay
