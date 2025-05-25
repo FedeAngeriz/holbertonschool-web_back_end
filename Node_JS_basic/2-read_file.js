b26b23fc9f6edea03a323a46c8597e6e69676020
FedeAngeriz <10086@holbertonstudents.com>
2025-05-25 18:34:33 -0300

    update file 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lineas = data.trim().split('\n');
+
+    let totalStudents = 0;
+    const fields = {};
+
+    for (let i = 0; i < lineas.length; i++) {
+      const linea = lineas[i].trim();
+
+      if (linea === '') continue;
+
+      if (i === 0) continue;
+
+      const parts = linea.split(',');
+      const firstname = parts[0];
+      const field = parts[parts.length - 1];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+      totalStudents++;
+    }
+
+    console.log(`Number of students: ${totalStudents}`);
+    for (const field in fields) {
+      const list = fields[field].join(', ');
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
+    }
+
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
